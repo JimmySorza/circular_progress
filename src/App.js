109efcc6fc3a2c8cf7cd072b25b787db419f5320
Jimmy Sorza <jimmysorza@gmail.com>
2019-12-13 10:53:56 -0600

    add components to app js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,  {useState}   from 'react';
+import Slider from "@material-ui/core/Slider";
+import CircularProgress from "./components/CircularProgress";
+import {ColorIndicator} from "./components/ColorIndicator";
 
 function App() {
+    const svgWidth = 150;
+    const arcWidth = 12;
+    const [progressPercentage, setProgressPercentage] = useState(50);
+    const colorIndicator = ColorIndicator(progressPercentage);
+    function valueText(value) {
+        return `${value}°C`;
+    }
+    function setProgressValue(event, value) {
+        setProgressPercentage(value)
+    }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <CircularProgress
+            svgWidth={svgWidth}
+            arcWidth={arcWidth}
+            progressPercentage={progressPercentage}
+            colorIndicator={colorIndicator}
+        />
+      <Slider
+        defaultValue={50}
+        getAriaValueText={valueText}
+        aria-labelledby='discrete-slider-small-steps'
+        step={10}
+        marks
+        min={0}
+        max={100}
+        valueLabelDisplay='auto'
+        onChange={(event, value) => {
+            setProgressValue(event, value)
+        }}
+      />
     </div>
   );
 }
